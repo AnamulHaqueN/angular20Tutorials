f078c20ae990bb188610084b12bcbf7efd9d4bab
AnamulHaqueN <rafaielanam@gmail.com>
2025-07-03 08:03:58 +0600

    add admin components & user components working of there in app.ts and also about standalone

---
+++ src/app/components/admin/admin.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  imports: [],
+  templateUrl: './admin.html',
+  styleUrl: './admin.css'
+})
+export class Admin {
+  firstName = 'Anamul';
+  courseName = 'Angular 20 Tutorials'
+  rollNo = 111
+}
