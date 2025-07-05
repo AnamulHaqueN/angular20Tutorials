12e276bec5be68a08af3b40480424f15e69f5172
AnamulHaqueN <rafaielanam@gmail.com>
2025-07-05 08:40:54 +0600

    Template form in Post API

--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user.html',
   styleUrl: './user.css'
 })
-export class User {
-  firstName = "Anu"
+export class User implements OnInit {
+  
+  userList: any[] = [];
+  userObj : any = {
+    "userId": 0,
+    "emailId": "",
+    "password": "",
+    "fullName": "",
+    "mobileNo": ""
+  };
+
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    debugger
+    this.getUsers()
+  }
+
+  getUsers() {
+    debugger
+    this.http.get("https://api.freeprojectapi.com/api/GoalTracker/getAllUsers").subscribe((result: any)=> {
+      debugger
+      this.userList = result;
+    })
+  }
+
+  registerUser() {
+    this.http.post("https://api.freeprojectapi.com/api/GoalTracker/register", this.userObj).subscribe({
+       next: (result)=> {
+        alert("Added Successful");
+        this.getUsers()
+       },
+       error:(error)=> {
+        alert("Error -" + error)
+       }
+    })
+  }
+
 }
